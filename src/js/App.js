2d68e7af0500f07d617b9e303a0a0c7f84653e62
Selim Asanov <asanovselim@mail.ru>
2016-11-23 00:08:53 +0300

    Deleted TabComponent.js

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,34 @@
 var React = require('react');
 var Chart = require('./Chart');
-var TabComponent = require('./TabComponent');
+var Tab = require('./Tab');
+var TabContainer = require('./TabContainer');
+var Table = require('./Table');
 var jsonData = require('../data/data');
 
 const Dashboard = React.createClass({
+    getKPIData: function (kpiName) {
+        return jsonData.summary.reduce(function (prev, cur) {
+            return prev || (cur.name === kpiName ? cur : prev);
+        },null);
+    },
     render: function () {
         return (
             <div>
               <Chart data={jsonData.summary[0].data} />
-              <TabComponent data={jsonData.summary} />
+              <TabContainer selected={0}>
+                  <Tab label="EBITDA">
+                      <Table data={this.getKPIData('EBITDA %')} />
+                  </Tab>
+                  <Tab label="Gross Margin">
+                      <Table data={this.getKPIData('Gross Margin')} />
+                  </Tab>
+                  <Tab label="Current Ratio">
+                      <Table data={this.getKPIData('Current Ratio')} />
+                  </Tab>
+                  <Tab label="Debt to Equity Ratio">
+                      <Table data={this.getKPIData('Debt to Equity Ratio')} />
+                  </Tab>
+              </TabContainer>
             </div>
         );
     }
